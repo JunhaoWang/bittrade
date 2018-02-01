a2421bbf5e529b89b61cfda24658888ca2b70a39
Lee <leodongxu@gmail.com>
2018-02-01 09:24:46 -0500

    modified the function and change the example function to the test function

--- src/bittrade/database/googleclient.py
+++ src/bittrade/database/googleclient.py
@@ -1,33 +1,80 @@
 from datetime import datetime
-
 import pandas as pd
 import requests
 
-
 def get_price_data(query):
-    r = requests.get(
-        "https://finance.google.com/finance/getprices", params=query,
-    )
-    lines = r.text.splitlines()
-    data = []
-    index = []
-    basetime = 0
-    for price in lines:
-        cols = price.split(",")
-        if cols[0][0] == 'a':
-            basetime = int(cols[0][1:])
-            index.append(datetime.fromtimestamp(basetime))
-            data.append([
-                float(cols[4]), float(cols[2]), float(
-                    cols[3],
-                ), float(cols[1]), int(cols[5]),
-            ])
-        elif cols[0][0].isdigit():
-            date = basetime + (int(cols[0])*int(query['i']))
-            index.append(datetime.fromtimestamp(date))
-            data.append([
-                float(cols[4]), float(cols[2]), float(
-                    cols[3],
-                ), float(cols[1]), int(cols[5]),
-            ])
-    return pd.DataFrame(data, index=index, columns=['Open', 'High', 'Low', 'Close', 'Volume'])
+	'''
+		Connected to google finance, get the price data, return to panda
+		Dataframe.
+	'''
+	r = requests.get(
+		"https://finance.google.com/finance/getprices", params=query,
+	)
+	lines = r.text.splitlines()
+	data = []
+	index = []
+	basetime = 0
+	for price in lines:
+		cols = price.split(",")
+		if cols[0][0] == 'a':
+			basetime = int(cols[0][1:])
+			index.append(datetime.fromtimestamp(basetime))
+			data.append([
+				float(cols[4]), float(cols[2]), float(
+					cols[3],
+				), float(cols[1]), int(cols[5]),
+			])
+		elif cols[0][0].isdigit():
+			date = basetime + (int(cols[0])*int(query['i']))
+			index.append(datetime.fromtimestamp(date))
+			data.append([
+				float(cols[4]), float(cols[2]), float(
+					cols[3],
+				), float(cols[1]), int(cols[5]),
+			])
+	return pd.DataFrame(data, index=index, columns=['Open', 'High', 'Low', 'Close', 'Volume'])
+
+def getClosePrice(target, period = '1Y'):
+	'''
+		Get the close price, for every target ticker.
+	'''
+	market, ticker = target.split(':')
+	param = {
+		'q': ticker,  # Bitcoin price in USD
+		'i': "86400",  # Interval size in seconds ("86400" = 1 day intervals)
+		'x': market,
+		'p': period,  # Period (Ex: "4Y" = 4 year)
+	}
+	df = get_price_data(param)
+	price = df[['Close']]
+	price = price.reset_index()
+	price['index'] = price['index'].apply(lambda x: x.date())
+	price.columns = ['Date', ticker]
+	return price
+
+def getClosePriceTable(targetList, interpolation = True, period = '1Y'):
+	'''
+		Function built on getClosePrice, this function take a list of tickers
+		and return to a panda dataframe, where every column is the closed price
+		for the ticker in the tickerList.
+		The interpolation option: if it is true, NAN value in the table with be
+		fitted using interpolation method.
+		period is the time window we want to trace back.
+	'''
+	df_table = pd.DataFrame()
+	for target in targetList:
+		price = getClosePrice(target, period)
+		if (df_table.shape == (0, 0)):
+			df_table = price
+		else:
+			df_table = pd.merge(df_table, price, how='outer', on='Date')
+	if (interpolation == False):
+		return df_table
+	else:
+		priceTable = df_table
+		# The interpolation method I use here is linear interpolation.
+		priceTable['Date'] = pd.to_datetime(priceTable.Date)
+		priceTable = priceTable.sort_values(by='Date')
+		priceTable = priceTable.interpolate(method='linear', axis=0).ffill().bfill()
+		priceTable = priceTable.set_index('Date')
+		return priceTable
--- src/bittrade/database/googleclient_test.py
+++ src/bittrade/database/googleclient_test.py
@@ -1,6 +1,4 @@
-# Running regression on daily close price based on four years data, data processing part.
-import pandas as pd
-from googleclient import get_price_data
+from googleclient import getClosePriceTable
 # Getting the date we need from google finance API
 # currencyTicker represents the bitcoin price, and the most tradable currency include Japanese Yen, Swiss Franc,
 # Australian Dollar, Canadian Dollar, Indian Rupee, Euro. The price data for these currency are all quoted in dollar.
@@ -39,41 +37,6 @@ etfTickers = [
     "NYSEARCA:FAZ", "NYSEARCA:GDX", "NYSEARCA:EWJ", "NYSEARCA:SPXU", "NYSEARCA:EWZ", "NYSEARCA:FAS",
     "NASDAQ:TVIX", "NYSEARCA:USO", "NYSEARCA:GLD", "NYSEARCA:TBT", "NYSEARCA:UNG",
 ]
-# Function use to get currency data and major stock index
 
-
-def getClosePrice(target):
-    market, ticker = target.split(':')
-    param = {
-        'q': ticker,  # Bitcoin price in USD
-        'i': "86400",  # Interval size in seconds ("86400" = 1 day intervals)
-        'x': market,
-        'p': "4Y",  # Period (Ex: "4Y" = 4 year)
-    }
-    df = get_price_data(param)
-    price = df[['Close']]
-    price = price.reset_index()
-    price['index'] = price['index'].apply(lambda x: x.date())
-    price.columns = ['Date', ticker]
-    return price
-
-
-def getClosePriceTable(targetList):
-    df_table = pd.DataFrame()
-    for target in targetList:
-        price = getClosePrice(target)
-        if (df_table.shape == (0, 0)):
-            df_table = price
-        else:
-            df_table = pd.merge(df_table, price, how='outer', on='Date')
-    return df_table
-
-
-# Get a joined table of all tickers sharing the same Data index
-priceTable = getClosePriceTable(currencyTickers + etfTickers)
-# The interpolation method I use here is linear interpolation.
-priceTable['Date'] = pd.to_datetime(priceTable.Date)
-priceTable = priceTable.sort_values(by='Date')
-priceTable = priceTable.interpolate(method='linear', axis=0).ffill().bfill()
-priceTable = priceTable.set_index('Date')
-priceTable
+table = getClosePriceTable(currencyTickers + etfTickers, interpolation = True, period = '1Y')
+print(table.head())
