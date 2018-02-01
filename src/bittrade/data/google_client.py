5807e7e635e238218388cf5e47cdacff4f154f53
junhao <junhao@umich.edu>
2018-02-02 02:12:55 +0800

    edit leo's commit

--- src/bittrade/data/google_client.py
+++ src/bittrade/data/google_client.py
@@ -1,13 +1,27 @@
+"""Module to retrieves Google Finance data.
+
+This module contains various price for stocks and crypto.
+
+Todo:
+    * EMPTY
+    * You have to also use ``sphinx.ext.todo`` extension
+"""
 from datetime import datetime
+
 import pandas as pd
 import requests
 
 
 def get_price_data(query):
-    '''
-        Connected to google finance, get the price data, return to panda
-        Dataframe.
-    '''
+    """
+    Use Google Finance to get data.
+
+    Args:
+        query (str): EMPTY
+
+    Returns:
+        pandas.DataFrame: EMPTY
+    """
     r = requests.get(
         "https://finance.google.com/finance/getprices", params=query,
     )
@@ -36,10 +50,17 @@ def get_price_data(query):
     return pd.DataFrame(data, index=index, columns=['Open', 'High', 'Low', 'Close', 'Volume'])
 
 
-def getClosePrice(target, period='1Y'):
-    '''
-        Get the close price, for every target ticker.
-    '''
+def get_close_price(target, period='1Y'):
+    """
+    Get the close price, for every target ticker.
+
+    Args:
+        target (EMPTY): EMPTY
+        period (str): EMPTY
+
+    Returns:
+        EMPTY: EMPTY
+    """
     market, ticker = target.split(':')
     param = {
         'q': ticker,  # Bitcoin price in USD
@@ -55,18 +76,13 @@ def getClosePrice(target, period='1Y'):
     return price
 
 
-def getClosePriceTable(targetList, interpolation=True, period='1Y'):
-    '''
-        Function built on getClosePrice, this function take a list of tickers
-        and return to a panda dataframe, where every column is the closed price
-        for the ticker in the tickerList.
-        The interpolation option: if it is true, NAN value in the table with be
-        fitted using interpolation method.
-        period is the time window we want to trace back.
-    '''
+def get_close_price_table(targetList, interpolation=True, period='1Y'):
+    """
+    EMPTY
+    """
     df_table = pd.DataFrame()
     for target in targetList:
-        price = getClosePrice(target, period)
+        price = get_close_price(target, period)
         if (df_table.shape == (0, 0)):
             df_table = price
         else:
@@ -75,7 +91,7 @@ def getClosePriceTable(targetList, interpolation=True, period='1Y'):
         return df_table
     else:
         priceTable = df_table
-        # The interpolation method I use here is linear interpolation.
+        # linear interpolation is applied.
         priceTable['Date'] = pd.to_datetime(priceTable.Date)
         priceTable = priceTable.sort_values(by='Date')
         priceTable = priceTable.interpolate(
